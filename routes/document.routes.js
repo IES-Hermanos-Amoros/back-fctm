9859f500d5126824e32a9aa5f5078310f886359d
Rosa <rosalindashi2005@gmail.com>
2026-01-08 19:31:00 +0100

    Rutas del Crud de Documentos

---
+++ routes/document.routes.js
@@ -0,0 +1,27 @@
+const documentController = require("../controllers/document.controller")                                                 
+const express = require("express")
+const router = express.Router()
+
+
+//Mostrar VISTA EJS index.ejs con listado de Documentos
+router.get("/",documentController.getAllDocuments)
+
+//Mostrar vista para crear un documento
+//router.get("/new",documentController.showNewComment) "VISTA EJS"
+
+//POST - Crear Documento
+router.post("/",documentController.newDocument)
+
+//Mostrar Documentos por ID
+router.get("/:id",documentController.getDocumentById)
+
+//Mostrar vista para editar un Documento
+//router.get("/:id/edit",documentController.showEditComment) "VISTA EJS"
+
+//PATCH - Updatear un Documento
+router.patch("/:id",documentController.editDocumentById)
+
+//DELETE - Borrar un Documento
+router.delete("/:id",documentController.deleteDocumentByID)
+
+
