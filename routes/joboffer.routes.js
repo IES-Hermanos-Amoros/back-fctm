8bc9ec66187c4e6909d8721259ce6afc360a6006
carpen06 <carlostorrespe06@gmail.com>
2026-01-08 19:07:09 +0100

    Job Offer routes terminado

---
+++ routes/joboffer.routes.js
@@ -0,0 +1,17 @@
+const jobOfferController = require("../controllers/joboffer.controller")
+const express = require("express")
+const router = express.Router()
+
+
+router.get("/",jobOfferController.findAllJobOffers)
+
+router.post("/",jobOfferController.postJobOffer)
+
+router.get("/:id",jobOfferController.findJobOfferById)
+
+router.patch("/:id",jobOfferController.editJobOffer)
+
+router.delete("/:id",jobOfferController.removeJobOffer)
+
+//Exportar rutas
+module.exports = router
