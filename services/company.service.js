4c9a589d46d539f91f4e8f491252538b2280a1fa
Daniel <danitorresegura@gmail.com>
2026-01-15 18:33:12 +0100

    Daniel - Correcion errores company.service

--- services/company.service.js
+++ services/company.service.js
@@ -11,14 +11,16 @@ exports.getById = async (id) => await userManagerModel.findById(id)
 exports.update = async (id,datos) => {
     const updateFields = {};
 
-    Object.keys(data).forEach(key => {
-        if (key.startsWith('FCTM_company_')) {
-            updateFields[key] = data[key];
+    Object.keys(datos).forEach(key => {
+        if (key.startsWith('FCTM_')) {
+            updateFields[key] = datos[key];
+        }else{
+            throw new Error('No se pueden actualizar los campos SAO_');
         }
     });
 
     if (Object.keys(updateFields).length === 0) {
-        throw new Error('No hay campos FCTM_company_ para actualizar');
+        throw new Error('No hay campos FCTM_ para actualizar');
     }
     return await userManagerModel.findByIdAndUpdate(id,datos,{new:true})
 }
