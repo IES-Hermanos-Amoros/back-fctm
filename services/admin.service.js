9be72aad433e43452a61cdb152652a09dd4c6049
Alex Mico <alexmico2006@gmail.com>
2026-01-15 19:54:12 +0100

    Arreglar bugs

--- services/admin.service.js
+++ services/admin.service.js
@@ -1,20 +1,25 @@
 const Admin = require('../models/userManager.model')
 
-exports.getAllAdmins = async () => await Admin.find({ SAO_profile: 'ADMINISTRADOR' })
+exports.getAllAdmins = async () =>
+  await Admin.find({ SAO_profile: 'ADMINISTRADOR' })
 
-
-exports.getAdminById = async id => await Admin.findOne({ _id: id, SAO_profile: 'ADMINISTRADOR' })
+exports.getAdminById = async id =>
+  await Admin.findOne({ _id: id, SAO_profile: 'ADMINISTRADOR' })
 
 exports.update = async (id, data) => {
   for (const campo in data) {
     if (Object.prototype.hasOwnProperty.call(data, campo)) {
       const regex = /^FCTM_/i
       if (!regex.test(campo)) {
-        throw new Error(`El campo '${campo}' no es válido. Debe empezar por FCTM_`)
+        throw new Error(
+          `El campo '${campo}' no es válido. Debe empezar por FCTM_`
+        )
       }
     }
   }
-  return await Admin.findOneAndUpdate({ _id: id, SAO_profile: 'ADMINISTRADOR' }, data, { new: true })
+  return await Admin.findOneAndUpdate(
+    { _id: id, SAO_profile: 'ADMINISTRADOR' },
+    data,
+    { new: true }
+  )
 }
-
-
