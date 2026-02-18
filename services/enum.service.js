478688621a355a6330929c9c9311818f99b40c43
Mariorob-06 <mrogal229@gmail.com>
2026-02-18 20:32:01 +0100

    ns

--- services/enum.service.js
+++ services/enum.service.js
@@ -3,9 +3,6 @@ const EnumeradoModel = require("../models/enum.js")
 exports.getAllEnums = () => {
     return EnumeradoModel
 }
-<<<<<<< HEAD
-exports.getEnumByName = async(enumName) => await EnumeradoModel.findOne({enum: enumName})
-=======
 
 exports.getEnumByName = async(enumName) => {
     const enumerado = EnumeradoModel[enumName]
@@ -16,4 +13,4 @@ exports.getEnumByName = async(enumName) => {
 
     return enumerado
 }
->>>>>>> e05a709f81243c60d56e656e46556f95ba609808
+
