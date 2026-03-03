e7ca8c7a74487fcc3b605973f4f08eeb931937c9
Carpen06 <carlostorrespe06@gmail.com>
2026-03-03 10:46:34 +0100

    Protección de rutas de job offers terminadas

--- routes/joboffer.routes.js
+++ routes/joboffer.routes.js
@@ -1,21 +1,25 @@
 const jobOfferController = require("../controllers/jobOffer.controller")
 const express = require("express")
 const router = express.Router()
+const { protect } = require("../middlewares/jwt.mw")
+const { restrictTo } = require("../middlewares/profile.mw")
+const { isOwner } = require("../middlewares/isOwner.mw")
+const userManager = require("../models/userManager.model")
 
 //Mostrar el listado de todos los job offers
-router.get("/",jobOfferController.findAllJobOffers)
+router.get("/",protect,restrictTo("ADMINISTRADOR","PROFESOR","ALUMNO"),jobOfferController.findAllJobOffers)
 
 //Crear un job offer
-router.post("/",jobOfferController.postJobOffer)
+router.post("/",protect,restrictTo("ADMINISTRADOR","PROFESOR","EMPRESA"),jobOfferController.postJobOffer)
 
 //Mostrar un job offer conseguido por id
-router.get("/:id",jobOfferController.findJobOfferById)
+router.get("/:id",protect,isOwner(userManager,"SAO_company_FCT_Number", ["ADMINISTRADOR", "PROFESOR","ALUMNO"]),jobOfferController.findJobOfferById)
 
 //Updatear un job offer
-router.patch("/:id",jobOfferController.editJobOffer)
+router.patch("/:id",protect,isOwner(userManager,"SAO_company_FCT_Number", ["ADMINISTRADOR", "PROFESOR"]),jobOfferController.editJobOffer)
 
 //Borrar un job offer
-router.delete("/:id",jobOfferController.deleteJobOffer)
+router.delete("/:id",protect,isOwner(userManager,"SAO_company_FCT_Number", ["ADMINISTRADOR", "PROFESOR"]),jobOfferController.deleteJobOffer)
 
 //Exportar rutas
 module.exports = router
