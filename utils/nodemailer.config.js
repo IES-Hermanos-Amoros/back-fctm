46eb598541347ad352833598462928d9b7395eda
sanchez.migben@gmail.com <sanchez.migben@gmail.com>
2026-03-05 13:17:19 +0100

    brevo config 2

--- utils/nodemailer.config.js
+++ utils/nodemailer.config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log(process.env.SMTP_HOST, process.env.SMTP_PORT, process.env.SMTP_USER, process.env.SMTP_PASS);
+//console.log(process.env.SMTP_HOST, process.env.SMTP_PORT, process.env.SMTP_USER, process.env.SMTP_PASS);
 
 // Creamos el transportador con los datos del .env
 export const transporter = nodemailer.createTransport({
@@ -11,7 +11,7 @@ export const transporter = nodemailer.createTransport({
   port: process.env.SMTP_PORT || 587,
   auth: {
     user: process.env.SMTP_USER || "a4109f001@smtp-brevo.com",
-    pass: process.env.SMTP_PASS || "xsmtpsib-a5c0e7261b7adb19f5ba96182576c9c433097f24cbd3f9b50c416e239fa35432-6U4oJpPHcFIgD1H5",
+    pass: process.env.SMTP_PASS || "xsmtpsib-a5c0e7261b7adb19f5ba96182576c9c433097f24cbd3f9b50c416e239fa35432-IH4xlZlEVkphs6QL",
   },
 });
 
